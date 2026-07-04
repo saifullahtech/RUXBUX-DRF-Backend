8304eea58fe6d19b4c1bfa84216dc84dfbc3a9c1
saifullahtech <contact@saifullahtech.com>
2026-07-04 21:31:52 +0500

    Update Django settings

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,9 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-43nel1(wpy=2mz1get^lh+=v4+cifk2$i8e1wc+&=6lar1_l2j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "ruxbux.com",
+    "www.ruxbux.com",
+    "46.202.93.99",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
@@ -119,17 +124,26 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
 CORS_ALLOWED_ORIGINS = [
+    "https://ruxbux.com",
+    "https://www.ruxbux.com",
+    "http://46.202.93.99",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
 ]
 
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+CSRF_TRUSTED_ORIGINS = [
+    "https://ruxbux.com",
+    "https://www.ruxbux.com",
+    "http://46.202.93.99",
+]
